f36a384423b5d5ff01f335c65b67d8e1c64e37db
Avadhut20 <your-email@example.com>
2024-07-02 10:47:00 +0530

    fixed

--- app/(auth)/sign-in/[[...sign-in]]/page.jsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.jsx
@@ -30,12 +30,11 @@ export default function Page() {
         </a>
 
         <h2 className="mt-6 text-2xl font-bold text-white sm:text-3xl md:text-4xl">
-          Welcome to Squid 🦑
+          Welcome to InterviewInsight 
         </h2>
 
         <p className="mt-4 leading-relaxed text-white/90">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi nam dolorum aliquam,
-          quibusdam aperiam voluptatum.
+        Prepare for interviews with our cutting-edge AI simulations that provide a realistic practice environment..
         </p>
       </div>
     </section>
